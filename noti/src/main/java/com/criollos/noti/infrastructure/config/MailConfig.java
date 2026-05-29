5b0dd9dda65f30e087e6f57f2b0b76443f8bf2ab
Crisbrix <cmateoramirez@ucundinamarca.edu.co>
2026-05-29 15:35:08 -0500

    config manual JavaMailSender con timeouts 15s

---
+++ noti/src/main/java/com/criollos/noti/infrastructure/config/MailConfig.java
@@ -0,0 +1,30 @@
+package com.criollos.noti.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("pdpm15042002@gmail.com");
+        mailSender.setPassword(System.getenv("MAIL_PASSWORD"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.connectiontimeout", "15000");
+        props.put("mail.smtp.timeout", "15000");
+        props.put("mail.smtp.writetimeout", "15000");
+
+        return mailSender;
+    }
+}
